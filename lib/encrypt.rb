0d3f0b2e775ee38836168bd49361c832f4a24785
David Stinnette <dastinnette@gmail.com>
2015-07-23 17:24:57 -0600

    Key and offset working in test file and program file

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -11,6 +11,10 @@ class Key
 
   attr_reader :key
 
+  def initialize(key = nil)
+    @key = key
+  end
+
   def generate_key
     num1 = rand(0..9)
     num2 = rand(0..9)
@@ -20,35 +24,64 @@ class Key
     @key = "#{num1}#{num2}#{num3}#{num4}#{num5}"
   end
 
-  def rot_a(key)
-    key[0..1]
-    # key_array = key.chars
-    # key_array[0..1].join
+  def rot_a
+    @key[0..1]
   end
 
-  def rot_b(key)
-    key[1..2]
+  def rot_b
+    @key[1..2]
   end
 
-  def rot_c(key)
-    key[2..3]
+  def rot_c
+    @key[2..3]
   end
 
-  def rot_d(key)
-    key[3..4]
+  def rot_d
+    @key[3..4]
   end
 
 end
 
 class Offset
-  
+
+  def initialize(offset = nil)
+    @offset = offset || find_offset
+  end
+
+  def find_offset
+    date = Time.now.strftime("%d%m%y")
+    squared = date.to_i**2
+    squared.to_s[-4..-1]
+  end
+
+  def off_a
+    @offset[-4]
+  end
+
+  def off_b
+    @offset[-3]
+  end
+
+  def off_c
+    @offset[-2]
+  end
+
+  def off_d
+    @offset[-1]
+  end
 end
 
-# if __FILE__ == $0
-# key = Key.new
-# p key.generate_key
-# p key.rot_a(key)
-# p key.rot_b(key)
-# p key.rot_c(key)
-# p key.rot_d(key)
-# end
+if __FILE__ == $0
+key = Key.new
+p key.generate_key
+p key.rot_a
+p key.rot_b
+p key.rot_c
+p key.rot_d
+
+offset = Offset.new
+p offset.off_a
+p offset.off_b
+p offset.off_c
+p offset.off_d
+end
--- test/encrypt_test.rb
+++ test/encrypt_test.rb
@@ -16,27 +16,54 @@ class EncryptTest < Minitest::Test
   end
 
   def test_rotation_a_returns_first_two_digits_of_key
-    unlock = Key.new
-    key = "12345"
-    assert_equal "12", unlock.rot_a(key)
+    unlock = Key.new("12345")
+    assert_equal "12", unlock.rot_a
   end
 
   def test_rotation_b_returns_second_and_third_digits_of_key
-    unlock = Key.new
-    key = "12345"
-    assert_equal "23", unlock.rot_b(key)
+    unlock = Key.new("12345")
+    assert_equal "23", unlock.rot_b
   end
 
   def test_rotation_c_returns_third_and_fourth_digits_of_key
-    unlock = Key.new
-    key = "12345"
-    assert_equal "34", unlock.rot_c(key)
+    unlock = Key.new("12345")
+    assert_equal "34", unlock.rot_c
   end
 
   def test_rotation_d_returns_fourth_and_fifth_digits_of_key
-    unlock = Key.new
-    key = "12345"
-    assert_equal "45", unlock.rot_d(key)
+    unlock = Key.new("12345")
+    assert_equal "45", unlock.rot_d
   end
 
+  def test_offset_is_a_four_digit_number
+    skip
+    offset = Offset.new
+    assert_equal 4, key.generate_key.length
+  end
+
+  def test_key_is_random_number
+    key1 = Key.new
+    key2 = Key.new
+    refute key1 == key2
+  end
+
+  def test_rotation_a_returns_first_two_digits_of_key
+    unlock = Key.new("12345")
+    assert_equal "12", unlock.rot_a
+  end
+
+  def test_rotation_b_returns_second_and_third_digits_of_key
+    unlock = Key.new("12345")
+    assert_equal "23", unlock.rot_b
+  end
+
+  def test_rotation_c_returns_third_and_fourth_digits_of_key
+    unlock = Key.new("12345")
+    assert_equal "34", unlock.rot_c
+  end
+
+  def test_rotation_d_returns_fourth_and_fifth_digits_of_key
+    unlock = Key.new("12345")
+    assert_equal "45", unlock.rot_d
+  end
 end
