f7e5b73adfd4a5498ef0ee936dc3789fa17165a2
David Stinnette <dastinnette@gmail.com>
2015-07-27 12:12:08 -0600

    Encrypt working

--- lib/encryptor.rb
+++ lib/encryptor.rb
@@ -4,6 +4,13 @@ class Encryptor
 
   attr_reader :a_rotation, :b_rotation, :c_rotation, :d_rotation
 
+  def initialize(a_rotation = 0, b_rotation = 0, c_rotation = 0, d_rotation = 0)
+    @a_rotation = a_rotation
+    @b_rotation = b_rotation
+    @c_rotation = c_rotation
+    @d_rotation = d_rotation
+  end
+
   def get_rotations
     calc = OffsetCalculator.new
     calc.get_key
@@ -22,62 +29,61 @@ class Encryptor
     characters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", " ", ".", "," ]
   end
 
-  def a_cipher(a_rotation)
+  def a_cipher
     characters
     a_rotated_characters = characters.rotate(a_rotation)
     Hash[characters.zip(a_rotated_characters)]
   end
 
-  def b_cipher(b_rotation)
+  def b_cipher
     characters
     b_rotated_characters = characters.rotate(b_rotation)
     Hash[characters.zip(b_rotated_characters)]
   end
 
-  def c_cipher(c_rotation)
+  def c_cipher
     characters
     c_rotated_characters = characters.rotate(c_rotation)
     Hash[characters.zip(c_rotated_characters)]
   end
 
-  def d_cipher(d_rotation)
+  def d_cipher
     characters
     d_rotated_characters = characters.rotate(d_rotation)
     Hash[characters.zip(d_rotated_characters)]
   end
 
-  def encrypt_letter_a(letter, a_rotation)
-    cipher_for_rotation = a_cipher(a_rotation)
+  def encrypt_letter_a(letter)
+    cipher_for_rotation = a_cipher
     cipher_for_rotation[letter]
   end
 
-  def encrypt_letter_b(letter, b_rotation)
-    cipher_for_rotation = b_cipher(b_rotation)
+  def encrypt_letter_b(letter)
+    cipher_for_rotation = b_cipher
     cipher_for_rotation[letter]
   end
 
-  def encrypt_letter_c(letter, c_rotation)
-    cipher_for_rotation = c_cipher(c_rotation)
+  def encrypt_letter_c(letter)
+    cipher_for_rotation = c_cipher
     cipher_for_rotation[letter]
   end
 
-  def encrypt_letter_d(letter, d_rotation)
-    cipher_for_rotation = d_cipher(d_rotation)
+  def encrypt_letter_d(letter)
+    cipher_for_rotation = d_cipher
     cipher_for_rotation[letter]
   end
 
-  def encrypt(string, a_rotation, b_rotation, c_rotation, d_rotation)
-    p string.length
-    array = string.split("")
+  def encrypt(string)
+    array = string.downcase.split("")
     fours = array.each_slice(4).to_a
     results = ""
     fours.each do |four|
-      results << encrypt_letter_a(four[0], a_rotation)
-      results << encrypt_letter_b(four[1], b_rotation)
-      results << encrypt_letter_c(four[2], c_rotation)
-      results << encrypt_letter_d(four[3], d_rotation)
+      results << encrypt_letter_a(four[0])
+      results << encrypt_letter_b(four[1])
+      results << encrypt_letter_c(four[2])
+      results << encrypt_letter_d(four[3])
     end
-    p results
+    results
   end
 
 end
@@ -85,5 +91,5 @@ end
 if __FILE__ == $0
 e = Encryptor.new
 e.get_rotations
-e.encrypt("1234", a_rotation, b_rotation, c_rotation, d_rotation)
+e.encrypt("This is my file to encrypt. It is a secret message that you will never be able to read. That is too bad.")
 end
--- test/encryptor_test.rb
+++ test/encryptor_test.rb
@@ -11,60 +11,66 @@ class EncryptorTest < Minitest::Test
   end
 
   def test_a_cipher_rotates_properly_according_to_rotation
-    rotation = Encryptor.new
-    assert_equal ["b"], rotation.a_cipher(1).values_at("a")
+    rotation = Encryptor.new(1, 0, 0, 0)
+    assert_equal ["b"], rotation.a_cipher.values_at("a")
   end
 
   def test_b_cipher_rotates_properly_according_to_rotation
-    rotation = Encryptor.new
-    assert_equal ["c"], rotation.b_cipher(2).values_at("a")
+    rotation = Encryptor.new(2, 2, 45, 39)
+    assert_equal ["c"], rotation.b_cipher.values_at("a")
   end
 
   def test_c_cipher_rotates_properly_according_to_rotation
-    rotation = Encryptor.new
-    assert_equal ["d"], rotation.c_cipher(3).values_at("a")
+    rotation = Encryptor.new(1, 33, 3, 54)
+    assert_equal ["d"], rotation.c_cipher.values_at("a")
   end
 
   def test_d_cipher_rotates_properly_according_to_rotation
-    rotation = Encryptor.new
-    assert_equal ["e"], rotation.d_cipher(4).values_at("a")
+    rotation = Encryptor.new(1, 4, 66, 4)
+    assert_equal ["e"], rotation.d_cipher.values_at("a")
   end
 
   def test_input_encrypts_a_properly_based_on_rotation
-    shift = Encryptor.new
-    assert_equal "b", shift.encrypt_letter_a("a", 1)
+    shift = Encryptor.new(1, 0, 0, 0)
+    assert_equal "b", shift.encrypt_letter_a("a")
   end
 
   def test_input_encrypts_b_properly_based_on_rotation
-    shift = Encryptor.new
-    assert_equal "c", shift.encrypt_letter_b("a", 2)
+    shift = Encryptor.new(2, 2, 45, 39)
+    assert_equal "c", shift.encrypt_letter_b("a")
   end
 
   def test_input_encrypts_c_properly_based_on_rotation
-    shift = Encryptor.new
-    assert_equal "d", shift.encrypt_letter_a("a", 3)
+    shift = Encryptor.new(1, 33, 3, 54)
+    assert_equal "d", shift.encrypt_letter_c("a")
   end
 
   def test_input_encrypts_d_properly_based_on_rotation
-    shift = Encryptor.new
-    assert_equal "e", shift.encrypt_letter_a("a", 4)
+    shift = Encryptor.new(1, 4, 66, 4)
+    assert_equal "e", shift.encrypt_letter_d("a")
   end
 
   def test_word_encrypts_properly_based_on_simple_4_part_cipher
     input = "1234"
-    encryptor = Encryptor.new
-    assert_equal "2345", encryptor.encrypt(input, 1, 1, 1, 1)
+    encryptor = Encryptor.new(1, 1, 1, 1)
+    assert_equal "2345", encryptor.encrypt(input)
   end
 
   def test_word_encrypts_properly_based_on_4_different_4_part_cipher
     input = "1111"
-    encryptor = Encryptor.new
-    assert_equal "2345", encryptor.encrypt(input, 1, 2, 3, 4)
+    encryptor = Encryptor.new(1, 2, 3, 4)
+    assert_equal "2345", encryptor.encrypt(input)
   end
 
   def test_word_encrypts_properly_based_on_complex_4_part_cipher
     input = "1111"
-    encryptor = Encryptor.new
-    assert_equal "p7j3", encryptor.encrypt(input, 27, 45, 99, 2)
+    encryptor = Encryptor.new(27, 45, 99, 2)
+    assert_equal "p7j3", encryptor.encrypt(input)
+  end
+
+  def test_input_string_length_equals_output_string_length
+    encryptor = Encryptor.new(1, 1, 1, 1)
+    string = "1111"
+    assert string.length == encryptor.encrypt(string).length
   end
 end
