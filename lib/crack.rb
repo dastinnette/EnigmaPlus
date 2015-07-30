783941c1a9b62df030b2791df82da3b8b3c6592d
David Stinnette <dastinnette@gmail.com>
2015-07-29 20:35:58 -0600

    cracked fucking worksgit status

--- lib/crack.rb
+++ lib/crack.rb
@@ -16,40 +16,129 @@
 # Rotation - 1 2 2 5 = key
 # Run result through decrypt function
 
-# Don't need to mess with reversing
-
+require 'pry'
 require_relative 'file_io'
+require_relative 'character_map'
 
 class Crack
 
-  attr_reader :message, :file_io
+  attr_reader :message, :file_io, :characters, :character_hash
 
   def initialize
     @file_io = FileIO.new
+    @characters = CharacterMap.new.characters
+    @character_hash = CharacterMap.new.character_hash
     @message = file_io.message("crack_test.txt")
   end
 
-  def message_length
-    message.chomp.length
+  def format_message
+    message.chomp
   end
 
   def find_modulo
-    message_length%4
+    format_message.length%4
   end
 
-  def final_rotation
+  # def find_rotation(known_character)
+  #   # look in the message file, grab last character = variable
+  #     crack_last = format_message[-1]
+  #   # find last character in character map and its index = variable
+  #     crack_last_index = characters.index(crack_last)
+  #   # find known character in the character map in the index = variable
+  #     known_last_index = characters.index(known_character)
+  #   # subtract known character index from encrypted character index = variable
+  #     rotation = known_last_index - crack_last_index
+  #   # repeat for all 4 characters
+  #
+  # end
+
+  def find_rotation(known_char, char_index)
+   char_to_crack = format_message[char_index]
+   char_to_crack_index = character_hash[char_to_crack]
+   known_char_index = character_hash[known_char]
+     if char_to_crack_index < known_char_index
+       rotation = character_hash.length + (char_to_crack_index - known_char_index)
+     elsif char_to_crack_index > known_char_index
+       rotation = (char_to_crack_index - known_char_index)
+     end
+  end
+
+  def rotation_one
+    find_rotation("n", -4)
+  end
+
+  def rotation_two
+    find_rotation("d", -3)
+  end
+
+  def rotation_three
+    find_rotation(".", -2)
+  end
+
+  def rotation_four
+    find_rotation(".", -1)
+  end
+
+  def final_index
     case find_modulo
     when 0
-      "D"
+      Hash["a_rotation", rotation_one, "b_rotation", rotation_two, "c_rotation", rotation_three, "d_rotation", rotation_four]
     when 1
-      "A"
+      Hash["b_rotation", rotation_one, "c_rotation", rotation_two, "d_rotation", rotation_three, "a_rotation", rotation_four]
     when 2
-      "B"
+      Hash["c_rotation", rotation_one, "d_rotation", rotation_two, "a_rotation", rotation_three, "b_rotation", rotation_four]
     when 3
-      "C"
+      Hash["d_rotation", rotation_one, "a_rotation", rotation_two, "b_rotation", rotation_three, "c_rotation", rotation_four]
     end
   end
+# because a is always assigned to rotation one, etc.
+  def crack_letter_a(letter)
+    input = final_index.fetch("a_rotation")
+    a_rotated_characters = characters.rotate(-input)
+    Hash[characters.zip(a_rotated_characters)][letter]
+  end
 
+  def crack_letter_b(letter)
+    input = final_index.fetch("b_rotation")
+    b_rotated_characters = characters.rotate(-input)
+    Hash[characters.zip(b_rotated_characters)][letter]
+  end
 
+  def crack_letter_c(letter)
+    input = final_index.fetch("c_rotation")
+    c_rotated_characters = characters.rotate(-input)
+    Hash[characters.zip(c_rotated_characters)][letter]
+  end
+
+  def crack_letter_d(letter)
+    input = final_index.fetch("d_rotation")
+    d_rotated_characters = characters.rotate(-input)
+    Hash[characters.zip(d_rotated_characters)][letter]
+  end
+
+  def crack(message)
+    i = 0
+    crack_arr = []
+    message_to_crack = message.downcase
+    while i < message_to_crack.length
+      if i % 4 == 0 || i == 0
+        crack_arr << crack_letter_a(message_to_crack[i])
+      elsif i % 4 == 1 || i == 1
+        crack_arr << crack_letter_b(message_to_crack[i])
+      elsif i % 4 == 2 || i == 2
+        crack_arr << crack_letter_c(message_to_crack[i])
+      elsif i % 4 == 3 || i == 3
+        crack_arr << crack_letter_d(message_to_crack[i])
+      end
+      i += 1
+      cracked_input = crack_arr.join
+    end
+    cracked_input
+  end
 
 end
+file_io = FileIO.new
+message = file_io.message
+c  = Crack.new
+# c.format_message
+file_io.output(c.crack(message))
--- test/crack_test.rb
+++ test/crack_test.rb
@@ -11,13 +11,6 @@ class CrackTest < Minitest::Test
     assert message
   end
 
-  def test_finds_length_of_string_to_crack
-    file_io = FileIO.new
-    message = file_io.message("crack_test.txt")
-    crack   = Crack.new
-    assert_equal 7, crack.message_length
-  end
-
   def test_returns_proper_modulo_four_of_string_length
     file_io = FileIO.new
     message = file_io.message("crack_test.txt")
@@ -29,8 +22,37 @@ class CrackTest < Minitest::Test
     file_io = FileIO.new
     message = file_io.message("crack_test.txt")
     crack   = Crack.new
-    assert_equal "C", crack.final_rotation
+    assert_equal [36, 11, 13, 25], crack.final_index
   end
 
-  def
+  def test_crack_finds_distance_from_known_character_to_encrypted
+    file_io = FileIO.new
+    message = file_io.message("crack_test.txt")
+    crack   = Crack.new
+    crack.rotation_one
+    known_char = "n"
+    char_index = -4
+    assert_equal "11", crack.find_rotation(known_char, char_index)
+  end
+
+  def test_crack_finds_distance_from_known_character_to_encrypted
+    file_io = FileIO.new
+    message = file_io.message("crack_test.txt")
+    crack   = Crack.new
+    crack.rotation_one
+    known_char = "n"
+    char_index = -4
+    assert_equal "11", crack.find_rotation(known_char, char_index)
+  end
+
+  # def test_rotation_one_works
+  #   file_io = FileIO.new
+  #   message = file_io.message("crack_test.txt")
+  #   crack   = Crack.new
+  #   known_char = "."
+  #   char_index =
+  #   assert_equal " ", crack.find_rotation(known_char, char_index)
+  # end
+
+
 end
